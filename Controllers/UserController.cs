8367c5af031bbf79f19da1e664e01c37d304e955
Wladimilson <wladimilsonmn@gmail.com>
2018-04-09 15:10:27 -0300

    Clear project

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -89,7 +89,7 @@ namespace app_test_jmeter.Controllers
         public async Task<IActionResult> Download(string PerfilImage)
         {
             if (PerfilImage == null)
-                return Content("filename not present");
+                return Content("Filename not present");
 
             var path = Path.Combine(filePath, PerfilImage);
 
@@ -125,86 +125,6 @@ namespace app_test_jmeter.Controllers
                 {".gif", "image/gif"},
                 {".csv", "text/csv"}
             };
-        }
-
-        // GET: User/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var user = await _context.AdmUsers.SingleOrDefaultAsync(m => m.Id == id);
-            if (user == null)
-            {
-                return NotFound();
-            }
-            return View(user);
-        }
-
-        // POST: User/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Age,Username,Password,PerfilImage")] User user)
-        {
-            if (id != user.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(user);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!UserExists(user.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(user);
-        }
-
-        // GET: User/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var user = await _context.AdmUsers
-                .SingleOrDefaultAsync(m => m.Id == id);
-            if (user == null)
-            {
-                return NotFound();
-            }
-
-            return View(user);
-        }
-
-        // POST: User/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var user = await _context.AdmUsers.SingleOrDefaultAsync(m => m.Id == id);
-            _context.AdmUsers.Remove(user);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
         }
 
         private bool UserExists(int id)
